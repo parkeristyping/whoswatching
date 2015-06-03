cf03a80f4aa551b5a900a100e4c0bec8e81a4c1b
Parker Lawrence <parker.alford@gmail.com>
2015-06-03 02:22:39 -0400

    fix controller

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,11 +1,13 @@
 class CounterController < WebsocketRails::BaseController
   def hello
-    @count = Viewer.increment_counter(:count,1)
+    Viewer.increment_counter(:count,1)
+    @count = Viewer.first.count
     send_message :update, @count
   end
 
   def goodbye
     @count = Viewer.decrement_counter(:count,1)
+    @count = Viewer.first.count
     send_message :update, @count
   end
 end
