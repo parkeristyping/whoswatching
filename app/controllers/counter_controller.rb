1d76e7baffd3ede59da6e94ddb5801f53528456c
Parker Lawrence <parker.alford@gmail.com>
2015-06-03 09:26:54 -0400

    add floor of zero to soft fix bug

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -2,14 +2,14 @@ class CounterController < WebsocketRails::BaseController
   def hello
     Viewer.increment_counter(:count,1)
     @count = Viewer.first.count
-    send_message :update, @count
     WebsocketRails[:updates].trigger(:update, @count)
   end
 
   def goodbye
-    Viewer.decrement_counter(:count,1)
+    unless Viewer.first.count == 0
+      Viewer.decrement_counter(:count,1)
+    end
     @count = Viewer.first.count
-    send_message :update, @count
     WebsocketRails[:updates].trigger(:update, @count)
   end
 end
